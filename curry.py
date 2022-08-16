1ae44c3a853c6f8c2ba21f52b732d767e03cf68d
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-08-17 01:08:12 +0900

    Modify curry take keyword argument
    Modify curry decorator that curried function can take keyword argument.

--- curry.py
+++ curry.py
@@ -6,9 +6,10 @@ __all__ = ["curry"]
 # https://medium.com/@aeshghi/functional-programming-python-curry-decorator-function-756fdae3036f
 def curry(wrapped_fn, arity=None):
     n_args = len(getfullargspec(wrapped_fn).args) if arity is None else arity
-    def curried(first_arg, *args):
-        if n_args == len(args) + 1:
-            return wrapped_fn(first_arg, *args)
+    def curried(*args, **kwargs):
+        len_args = len(args) + len(kwargs)
+        if n_args == len_args:
+            return wrapped_fn(*args, **kwargs)
 
-        return curry(partial(wrapped_fn, first_arg, *args), n_args - (1 + len(args)))
+        return curry(partial(wrapped_fn, *args, **kwargs), n_args - len_args)
     return curried
