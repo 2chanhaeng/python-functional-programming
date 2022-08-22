bf8e08cdda7f416a18f1693608a3b6992e9e55be
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-08-23 04:52:20 +0900

    Create true, false, first, second

--- lambda_calculus.py
+++ lambda_calculus.py
@@ -1,11 +1,14 @@
 from recursive import recursive
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, ParamSpec
 
-__all__ = ["succ", "plus", "mult", "exp", "pred", "minus"]
-T = TypeVar('T')
+__all__ = ["succ", "plus", "mult", "exp", "pred", "minus", "true", "false", "pair", "first", "second"]
+
+T, _S = TypeVar('T'), TypeVar('S')
+P, Q = ParamSpec('P'), ParamSpec('P')
 F = Callable[[T], T]
 Functor = Callable[[F], F]
 Recusive = int
+Pair = Callable[P,Callable[Q,Callable[[Callable[P,Callable[Q,T]]],T]]]
 
 # https://en.wikipedia.org/wiki/Church_encoding#Table_of_functions_on_Church_numerals
 
@@ -26,3 +29,13 @@ pred:Callable[[Recusive], Callable[[F], Callable[[T], T]]] \
 
 minus:Callable[[Recusive], Callable[[Recusive], Functor]]\
      = lambda m: lambda n: recursive(n)(pred)(recursive(m))
+
+true :Callable[[T], Callable[[_S], T]] = lambda a: lambda b: a
+
+false:Callable[[T], Callable[[_S], _S]] = lambda a: lambda b: b
+
+pair:Pair = lambda a: lambda b: lambda h: h(a)(b)
+
+first :Callable[[Pair],T] = lambda p: p(true)
+
+second:Callable[[Pair],T] = lambda p: p(false)
