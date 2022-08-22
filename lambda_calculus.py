9a712c61a589195d8522c38b44f61c019636883a
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-08-23 02:47:56 +0900

    Make functions in lambda_calculus take int
    Functions take int and call `recursive` in function.

--- lambda_calculus.py
+++ lambda_calculus.py
@@ -1,27 +1,28 @@
+from recursive import recursive
 from typing import Callable, TypeVar
 
 __all__ = ["succ", "plus", "mult", "exp", "pred", "minus"]
 T = TypeVar('T')
 F = Callable[[T], T]
 Functor = Callable[[F], F]
-Recusive = Callable[[F, int], F]
+Recusive = int
 
 # https://en.wikipedia.org/wiki/Church_encoding#Table_of_functions_on_Church_numerals
 
 succ:Callable[[Recusive], Callable[[F], T]] \
-    = lambda n: lambda f: lambda x: f(n(f)(x))
+    = lambda n: lambda f: lambda x: f(recursive(n)(f)(x))
 
 plus:Callable[[Recusive], Callable[[Recusive], Callable[[F], Callable[[T], T]]]] \
-    = lambda m: lambda n: lambda f: lambda x: m(f)(n(f)(x)) # m(succ)(n)
+    = lambda m: lambda n: lambda f: lambda x: recursive(m)(f)(recursive(n)(f)(x)) # m(succ)(n)
 
 mult:Callable[[Recusive], Callable[[Recusive], Functor]] \
-    = lambda m: lambda n: lambda f: m(n(f))
+    = lambda m: lambda n: lambda f: recursive(m)(recursive(n)(f))
 
 exp:Callable[[Recusive], Callable[[Recusive], Functor]] \
-    = lambda m: lambda n: n(m)
+    = lambda m: lambda n: recursive(n)(recursive(m))
 
 pred:Callable[[Recusive], Callable[[F], Callable[[T], T]]] \
-    = lambda n: lambda f: lambda x: n(lambda g: lambda h: h(g(f)))(lambda u: x)(lambda u: u)
+    = lambda n: lambda f: lambda x: recursive(n)(lambda g: lambda h: h(g(f)))(lambda u: x)(lambda u: u)
 
 minus:Callable[[Recusive], Callable[[Recusive], Functor]]\
-     = lambda m: lambda n: n(pred)(m)
+     = lambda m: lambda n: recursive(n)(pred)(recursive(m))
